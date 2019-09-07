65d3b113092b2408c5e7511833899dab239a4ae1
Wojciech Sudol <sudol.wojciech@gmail.com>
2019-09-07 10:55:17 +0200

    Add simple state for selected products

--- src/app/product-menu/product-item/product-item.component.ts
+++ src/app/product-menu/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product.model'
+import { selectedProducts } from '../../state/selected-products';
 
 @Component({
     selector: "app-product-item",
@@ -8,5 +9,13 @@ import { Product } from '../../models/product.model'
 })
 export class ProductItem{
     @Input() product: Product;
+
+    /*
+        This function is supposed to be called by the '+' button'.
+        It adds the selected product to global list of selected products.
+    */
+    addSelectedProduct = (product: Product) => {
+        selectedProducts.push(product);
+    }
 }
 
